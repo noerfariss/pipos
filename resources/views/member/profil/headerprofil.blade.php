8bcaa8d08552e26d646866d002548c44bd7a671e
NUR FARIS PRASTYO <noerfaris@gmail.com>
2024-02-02 15:12:18 +0700

    remove header aktivitas

--- resources/views/member/profil/headerprofil.blade.php
+++ resources/views/member/profil/headerprofil.blade.php
@@ -6,7 +6,4 @@
     <li class="nav-item">
         <a class="nav-link {{ menuAktifProfil('password') }}" href="{{ route('password.index') }}"><i class='bx bxs-key'></i> Ganti Password</a>
     </li>
-    <li class="nav-item">
-        <a class="nav-link {{ menuAktifProfil('aktivitas') }}" href=""><i class='bx bx-bar-chart'></i> Aktivitas</a>
-    </li>
 </ul>
